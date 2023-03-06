93d8bab4056ad6495bee3a4251475cc7538905a6
Alexander Shevchenko <acidi@icloud.com>
2023-03-06 15:14:52 -0800

    add 'on file' handler

--- src/handlers/formdata.ts
+++ src/handlers/formdata.ts
@@ -1,3 +1,5 @@
+// https://github.com/mscdex/busboy
+
 export const parseFormData = (req, res) => {
   const data = [];
 
@@ -5,6 +7,28 @@ export const parseFormData = (req, res) => {
   req.busboy.on('field', (name, value, info) => {
     console.log(`Field [${name}]: value: %j`, value);
     data.push({ name, value });
+  });
+
+  req.busboy.on('file', (name, file, info) => {
+    const { filename, encoding, mimeType } = info;
+    let fileSize = 0;
+
+    console.log(
+      `File [${name}]: filename: %j, encoding: %j, mimeType: %j`,
+      filename,
+      encoding,
+      mimeType
+    );
+
+    file
+      .on('data', (data) => {
+        fileSize += data.length;
+        console.log(`File [${name}] got ${data.length} bytes`);
+      })
+      .on('close', () => {
+        console.log(`File [${name}] done`);
+        data.push({ name, fileSize: `${(fileSize / 1024).toFixed(2)} kb` });
+      });
   });
 
   req.busboy.on('finish', () => {
